281f43321427eebd4705c55539a2c6e04e2664d3
ClaudineRas02 <hanitraclaudine@gmail.com>
2025-10-16 15:04:35 +0300

    Finition et correction method pour le login

--- Backend/controllers/loginController.js
+++ Backend/controllers/loginController.js
@@ -1,19 +1,21 @@
 /**
  * param :  req.body contenant password et username
- * retour : res avec resultat du comparaison message et/ou token de checkInfoLoginService
+ * retour : res avec resultat du comparaison, message et/ou token de checkInfoLoginService
  */
 
 import {checkInfoLoginService} from "../services/loginService.js";
 
 export async function checkInfoLoginController(req,res){
     try {
-        const result = await checkInfoLoginService(req.query);
-        if (result.ok) {
+
+        const result = await checkInfoLoginService(req.body);
+
+        if(result.ok) {
             res.status(200).json({message: result.message, ok : result.ok, token: result.token });
-        } else {
+        }else {
             res.status(200).json({ message: result.message,  ok : result.ok, });
         }
     } catch (error) {
-        res.status(error.status).json({ message: error.message });
+        res.status(error.status||500).json({ message: error.message });
     }
 }
