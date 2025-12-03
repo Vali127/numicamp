6686c4195676125e283130c1c258d6fdc205be82
valimbavaka <mh-valimbavaka@gmail.com>
2025-12-03 10:49:09 +0300

    chore : renaming some files + refactoring some code

--- Frontend/src/view/section/ressources/resources.jsx
+++ Frontend/src/view/section/ressources/resources.jsx
@@ -2,8 +2,8 @@ import {UnderDev} from "../../tempComponent/UnderDev.jsx";
 
 export const Resources = () => {
     return (
-        <div className={"h-full"}>
-            <UnderDev name="resources" />
+        <div className={"h-full mx-3"}>
+            <div className="flex items-center gap-3 text-2xl font-bold mb-4"><label className="icon_btn">&#xE28C;</label><label>Ressources</label></div>
         </div>
     )
 }
