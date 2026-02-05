bc781f2942ac923a49fa92d06b5607a64b5b1516
valimbavaka <mh.valimbavaka@gmail.com>
2026-02-05 06:30:39 +0300

    chore : nettoyage du couche sur l' endpoint <<login>>

--- Backend/models/loginModel.js
+++ Backend/models/loginModel.js
@@ -1,7 +1,3 @@
-/**
- * param : mail por chercher le password
- * retour : objet contenant password,role et id pour la comparaison et token
- */
 import {pool} from "../config/db.js";
 
 export async function getLoginInfo(username){
@@ -17,71 +13,43 @@ export async function getLoginInfo(username){
         const [row] = await pool.query(sql, [username, username]);
         return row[0];
     }catch(error){
-        throw new Error("Erreur dans le model :"+error.message);
+        throw new Error("Erreur dans le model : "+error.message);
     }
 }
 
 export async function getAccountUsage(username) {
-    let connection
     try {
-        const connection = await pool.getConnection()
-        await connection.beginTransaction()
-        //Voir si admin
+        //CHECK IF ADMIN
         const stmt = `SELECT nom_profil FROM personne WHERE id_role = "admin" AND nom_profil = ? `
-        const [res_1] = await connection.query(stmt, [username]);
-        if ( res_1.length > 0 && res_1[0].nom_profil === username ){
-            return "admin"
-        }
+        const [res_1] = await pool.query(stmt, [username]);
+        if ( res_1.length > 0 && res_1[0].nom_profil === username ) { return "admin" }
 
         const sql = ` SELECT id_profil FROM personne WHERE nom_profil = ? `
-        const result = await  connection.query(sql, [username])
-        await connection.commit()
-
+        const result = await pool.query(sql, [username])
         const res = result[0]
 
-        if (res.length === 0 )
-            return "organisational"
-        return "personal"
+        return (res.length === 0 ) ? "organisational" : "personal"
     }
     catch(error) {
-        if (connection) await connection.rollback()
-        console.log(error)
-    }
-    finally {
-        if (connection)  connection.release()
+        throw Error(error);
     }
 }
 
-
-
-
 export async function getAccountUsageById(id) {
-    let connection
     try {
-        const connection = await pool.getConnection()
-        await connection.beginTransaction()
-        //Voir si admin
+        // CHECK ADMINS
         const stmt = `SELECT id_profil FROM personne WHERE id_role = "admin" AND id_profil = ? `
-        const [res_1] = await connection.query(stmt, [id]);
-        await connection.commit()
+        const [res_1] = await pool.query(stmt, [id]);
         if ( res_1.length > 0 && res_1[0].id_profil === id ){ return "admin" }
 
         const sql = ` SELECT id_profil FROM personne WHERE id_profil = ? `
-        const result = await  connection.query(sql, [id])
-        await connection.commit()
-
+        const result = await  pool.query(sql, [id])
         const res = result[0]
 
-        if (res.length === 0 )
-            return "organisational"
-        return "personal"
+        return (res.length === 0 ) ? "organisational" : "personal"
     }
     catch(error) {
-        if (connection) await connection.rollback()
-        console.log(error)
-    }
-    finally {
-        if (connection)  connection.release()
+        throw Error(error);
     }
 }
 
--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,6 @@ import {pool} from './config/db.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 dotenv.config();
 
 const app = express();
