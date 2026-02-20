49daa652fa2c75ebfea9172c8efdb9de7a650b15
valimbavaka <mh.valimbavaka@gmail.com>
2026-02-20 21:24:00 +0300

    feat : lazy loading + tansTackProvider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import TanstackProvider from "./provider/tanstack.provider.tsx";
 
 const Presentation = lazy(() => import("./view/accessibility/presentation.jsx").then(m => ({default : m.Presentation})));
 const MainEntry = lazy(() => import("./view/main.entry.jsx").then(m => ({default : m.MainEntry})));
@@ -8,14 +9,16 @@ const MainEntry = lazy(() => import("./view/main.entry.jsx").then(m => ({default
 function App() {
     return (
         <div>
-            <BrowserRouter>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path="/*" element={<Presentation />} />
-                        <Route path="/Main/*" element={<MainEntry />} />
-                    </Routes>
-                </Suspense>
-            </BrowserRouter>
+            <TanstackProvider>
+                <BrowserRouter>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path="/*" element={<Presentation />} />
+                            <Route path="/Main/*" element={<MainEntry />} />
+                        </Routes>
+                    </Suspense>
+                </BrowserRouter>
+            </TanstackProvider>
         </div>
     );
 }
